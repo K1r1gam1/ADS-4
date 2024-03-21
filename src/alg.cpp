5301fc912ee0c9a20a3d234116e86993fc62c51d
Evgeniy <160057044+K1r1gam1@users.noreply.github.com>
2024-03-21 19:57:13 +0300

    B56_ADS4_v2.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,51 +1,51 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  int k = 0;
-  for (int i = 0; i < len - 1; i++)
-    for (int j = i + 1; j < len; j++)
+int countPairs1(int *arr, int size, int value) {
+  int count = 0;
+  for (int i = 0; i < size - 1; i++)
+    for (int j = i + 1; j < size; j++)
       if ((arr[i] + arr[j]) == value)
-        k += 1;
-  return k;
+        count += 1;
+  return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  int k = 0;
-  for (int i = 0; i < len - 1; i++) {
+int countPairs2(int *arr, int size, int value) {
+  int count = 0;
+  for (int i = 0; i < size - 1; i++) {
     if (arr[i] > value)
       continue;
-    for (int j = len - 1; j > i; j--) {
+    for (int j = size - 1; j > i; j--) {
       if (arr[j] > value)
         continue;
       if ((arr[i] + arr[j]) == value)
-        k += 1;
+        count += 1;
     }
   }
-  return k;
+  return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  int k = 0;
-  for (int i = 0; i < len - 1; i++) {
-      int start = i, end = len, nado = value - arr[i];
-      while ((end - start) > 1) {
-          int mid = (start + end) / 2;
-          if (arr[mid] == nado) {
-              k++;
+int countPairs3(int *arr, int size, int value) {
+  int count = 0;
+  for (int i = 0; i < size - 1; i++) {
+      int left = i, right = size, n = value - arr[i];
+      while ((right - left) > 1) {
+          int mid = (left + right) / 2;
+          if (arr[mid] == n) {
+              count++;
               int top = mid + 1;
-              while (arr[top] == nado && top < end) {
+              while (arr[top] == n && top < right) {
                   k++;
                   top++;
               }
               top = mid - 1;
-              while (arr[top] == nado && top > start) {
-                  k++;
+              while (arr[top] == n && top > left) {
+                  count++;
                   top--;
               }
               break;
           }
-          if (arr[mid] > nado)
-              end = mid;
-          if (arr[mid] < nado)
-              start = mid;
+          if (arr[mid] > n)
+              right = mid;
+          if (arr[mid] < n)
+              left = mid;
       }
   }
-  return k;
+  return count;
 }
